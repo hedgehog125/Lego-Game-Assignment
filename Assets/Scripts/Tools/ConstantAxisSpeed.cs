01c203b8731619fb6f2e701581d8e84534add7de
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-10-04 18:04:35 +0100

    Got some basic mesh combining working

---
+++ Assets/Scripts/Tools/ConstantAxisSpeed.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ConstantAxisSpeed : MonoBehaviour {
+	private enum Axis {
+		X,
+		Y,
+		Z
+	}
+	[SerializeField] private Axis m_axis;
+	[SerializeField] private float m_amount;
+
+	private Rigidbody rb;
+	private void Awake() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		Vector3 vel = rb.velocity;
+		if (m_axis == Axis.X) vel.x = m_amount;
+		else if (m_axis == Axis.Y) vel.y = m_amount;
+		else vel.z = m_amount;
+
+		rb.velocity = vel;
+	}
+}
