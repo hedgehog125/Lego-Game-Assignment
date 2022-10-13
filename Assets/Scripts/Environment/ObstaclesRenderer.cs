3f3e5ef3ae2646b2a4be2ffaa61281c4d88c3fc0
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-10-13 08:22:20 +0100

    The ground is now combined in with the obstacle collider

--- Assets/Scripts/Environment/ObstaclesRenderer.cs
+++ Assets/Scripts/Environment/ObstaclesRenderer.cs
@@ -6,6 +6,10 @@ namespace Environment {
 	public class ObstaclesRenderer : MonoBehaviour {
 		[SerializeField] private float m_tileSize;
 
+		[Header("Collision")]
+		[SerializeField] private Mesh m_groundMesh;
+		[SerializeField] private Transform m_groundTran;
+
 		private class VisibleObject {
 			public GameObject ob;
 			public int tileID;
@@ -54,14 +58,14 @@ namespace Environment {
 		private void UpdateCollider() {
 			Mesh mesh = new Mesh();
 
-			CombineInstance[] combine = new CombineInstance[transform.childCount];
+			CombineInstance[] combine = new CombineInstance[transform.childCount + 1];
+			combine[0] = new CombineInstance {
+				mesh = m_groundMesh,
+				transform = m_groundTran.localToWorldMatrix
+			};
 			int i = 0;
 			foreach (VisibleObject visOb in visibleObjects) {
-				if (visOb.ob == null) {
-					visibleObjects.Remove(visOb);
-					continue;
-				}
-				combine[i] = new CombineInstance {
+				combine[i + 1] = new CombineInstance {
 					mesh = obstacles[visOb.tileID].collider,
 					transform = visOb.ob.transform.localToWorldMatrix
 				};
